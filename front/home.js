aa41f00cebe96b3f1034c8084ec1cbfaca57199e
Ankit2708 <shivhare.ankit476@gmail.com>
2022-06-13 14:30:30 +0530

    realtimechat

--- front/home.js
+++ front/home.js
@@ -1,13 +1,25 @@
 const messagebtn=document.getElementById('addmsgbtn')
 const messageinput=document.getElementById('messageinput')
-document.addEventListener('DOMContentLoaded',()=>{
-    axios.get('http://localhost:3000/getmessage').then(data=>{
-        const dataa=data.data
-        const message_cont_ul=document.getElementById('message_cont_ul')
-        for(let i=0;i<dataa.length;i++){
-            message_cont_ul.innerHTML=message_cont_ul.innerHTML+`<li>${dataa[i].username}:${dataa[i].msg}</li>`
-        }
-    })
+document.addEventListener("DOMContentLoaded",async()=>{
+    getMessages()
+})
+function getMessageApiCall(){
+    setInterval(()=>{
+        getMessages()
+    },1000)
+}
+getMessageApiCall()
+document.addEventListener('DOMContentLoaded',async()=>{
+    function getMessages(){
+        axios.get('http://localhost:3000/getmessage').then(data=>{
+            const dataa=data.data
+            const message_cont_ul=document.getElementById('message_cont_ul')
+            for(let i=0;i<dataa.length;i++){
+                message_cont_ul.innerHTML=message_cont_ul.innerHTML+`<li>${dataa[i].username}:${dataa[i].msg}</li>`
+            }
+        })
+    }
+    
 })
 messagebtn.addEventListener('click',(e)=>{
     e.preventDefault()
